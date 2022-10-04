f41e9e7954a1770c03c928b4bc39a3d069cfb967
Monica <jay.m.howard@gmail.com>
2022-10-03 19:31:06 -0500

    used querySelector to create dom elements

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputEl = document.querySelector("#bill-amount");
+let buttonEl = document.querySelector("#calculate-tip");
+let tipAmountEl = document.querySelector("#tip-amount");
+
+buttonEl.addEventListener("click", );
